ec8dae32cb5e6eea614d3ee45029be7f638ff5ec
Toby Fee <tobyfee@gmail.com>
2015-07-21 15:30:09 -0700

    first pass at Gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+ shell = require('gulp-shell')
+
+gulp.task('code', shell.task([
+  'node index.js'
+]))
+
+gulp.task('watch', function() {
+  // Watch .js files
+  gulp.watch('*.js', ['code']);
+
+});
--- index.js
+++ index.js
@@ -4,9 +4,38 @@ url = require('url')
 wrench = require('wrench')
 extractor = require('unfluff')
 
-crawler.crawl("http://tobyfee.com/")
-    .on("fetchcomplete", function(queueItem, responseBuffer, response){
+
+
+var downloadSite = function (domain, callback){
+  var outputDirectory = process.cwd() + '/' + domain
+  var teotihuacan = new crawler(domain)
+
+  //config block for the crawler
+  teotihuacan.downloadUnsupported = true
+  teotihuacan.interval = 250
+
+
+  teotihuacan.on("fetchcomplete", function(queueItem, responseBuffer, response){
         console.log("Completed fetching resource:", queueItem.url)
-        text = extractor(responseBuffer)
-        console.log(text.text)
-    })
+        var blobText = ""
+        parsedURL = url.parse(queueItem.url)
+        pathname = (parsedURL.pathname)
+        item = extractor(responseBuffer)
+        blobText = "Pathname:"+pathname + "\n" + item.text
+        console.log (blobText)
+  })
+
+  teotihuacan.on('complete', function () {
+    callback()
+  })
+  // Start Crawl
+  teotihuacan.start()
+}
+
+if (process.argv.length < 3) {
+  console.log('Usage: node index.js mysite.com')
+  process.exit(1)
+}
+downloadSite (process.argv[2], function () {
+  console.log ("done!")
+})
