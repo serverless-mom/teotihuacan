85c5ec37e8ae8880cc45d75df28c4fbfc60ead5f
Toby Fee <tobyfee@gmail.com>
2015-07-21 17:33:17 -0700

    MVP

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-crawler = require('simplecrawler')
-fs = require('node-fs')
-url = require('url')
-wrench = require('wrench')
-extractor = require('unfluff')
+var crawler = require('simplecrawler'),
+  fs = require('node-fs'),
+  url = require('url'),
+  wrench = require('wrench'),
+  extractor = require('unfluff')
 
 
 
 var downloadSite = function (domain, callback){
-  var outputDirectory = process.cwd() + '/' + domain
-  var teotihuacan = new crawler(domain)
+  outputFile = domain + ".txt"
+  fs.exists(outputFile, function(exists){
+    if (exists){
+      console.log ("file "+outputFile+" already exists! adding a timecode")
+      var now = new Date()
+      outputFile = outputFile + now.toString()
+    }
+  })
 
+  var teotihuacan = new crawler(domain)
   //config block for the crawler
   teotihuacan.downloadUnsupported = true
   teotihuacan.interval = 250
 
 
-  teotihuacan.on("fetchcomplete", function(queueItem, responseBuffer, response){
+  teotihuacan.on("fetchcomplete", function(queueItem, responseBuffer){
         console.log("Completed fetching resource:", queueItem.url)
-        var blobText = ""
+        var blobText = ""         //holds the page text and pathname
         parsedURL = url.parse(queueItem.url)
         pathname = (parsedURL.pathname)
         if (pathname === '/')
           pathname = '/index.html'
         item = extractor(responseBuffer)
-        blobText = "Pathname:"+pathname + "\n" + item.text
+        blobText = "#Pathname:"+pathname + "\n" + item.text +"\n"
         console.log (blobText)
+        fs.appendFile(outputFile, blobText, function (err) {
+          if (err) throw err;
+          console.log('The "data to append" was appended to file!');
+        });
   })
 
   teotihuacan.on('complete', function () {
@@ -39,5 +50,5 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 downloadSite (process.argv[2], function () {
-  console.log ("done!")
+  //console.log ("done!")
 })
